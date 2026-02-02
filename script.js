d08c9739efee82d7c6391f6e99eb18dadbf50f27
Aravind Vallaban <aravindvallab@outlook.com>
2026-02-02 20:16:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,34 @@ const noBtn = document.getElementById("noBtn");
 const message = document.getElementById("message");
 
 yesBtn.addEventListener("click", () => {
-  message.innerHTML = "Yayyy! 💖 I knew you'd say YES 😍";
+  message.innerHTML = "Yayyy! 💖 Redirecting…";
+
+  createHearts();
+
+  setTimeout(() => {
+    window.location.href = "https://www.instagram.com/samritivishwakarma/";
+  }, 2000);
 });
 
 noBtn.addEventListener("mouseover", () => {
   const x = Math.random() * 200 - 100;
   const y = Math.random() * 200 - 100;
-
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
+
+function createHearts() {
+  for (let i = 0; i < 15; i++) {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "💖";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = 2 + Math.random() * 2 + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 3000);
+  }
+}
